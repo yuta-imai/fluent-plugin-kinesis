d2a606cb281d5c970c6ddfc6a12371839f3a6af7
Yuta Imai <imai.factory@gmail.com>
2013-12-01 01:57:49 +0000

    skelton

--- fluent-plugin-kinesis.gemspec
+++ fluent-plugin-kinesis.gemspec
@@ -1,7 +1,6 @@
 # coding: utf-8
 lib = File.expand_path('../lib', __FILE__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
-require 'fluent/plugin/kinesis/version'
 
 Gem::Specification.new do |spec|
   spec.name          = "fluent-plugin-kinesis"
@@ -20,4 +19,7 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency "bundler", "~> 1.3"
   spec.add_development_dependency "rake"
+
+  spec.add_dependency "fluentd"
+  spec.add_dependency "aws-sdk"
 end
--- lib/fluent/plugin/out_kinesis.rb
+++ lib/fluent/plugin/out_kinesis.rb
@@ -1,4 +1,36 @@
 module Fluent
     class KinesisOutput < Fluent::BufferedOutput
+        Fluent::Plugin.register_output('kinesis',self)
+
+        def initialize
+            super
+            require 'aws-sdk'
+        end
+
+        def configure(conf)
+            super
+            @stream = conf['stream']
+            @path = conf['path']
+        end
+
+        def start
+            super
+        end
+
+        def shutdown
+            super
+        end
+
+        def format(tag,time,record)
+            [tag,time,record].to_msgpack
+        end
+
+        def write(chunk)
+            records = []
+            chunk.msgpack_each { |record|
+                # records << record
+            }
+            # write records
+        end
     end
 end
